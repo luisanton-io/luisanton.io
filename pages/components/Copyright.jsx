ec420b749ce3aaf1988e307e7d1731c957a5dd3d
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2022-01-10 10:28:33 +0100

    Update Copyright.jsx

--- pages/components/Copyright.jsx
+++ pages/components/Copyright.jsx
@@ -2,6 +2,6 @@ import styles from '../../styles/index.module.scss'
 
 export default function Copyright() {
     return <div className={styles.footer}>
-        Copyright © 2021 luisanton.io - All rights reserved
+        Copyright © {(new Date()).getFullYear()} luisanton.io - All rights reserved
     </div>
 }
