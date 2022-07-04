37abf55caadacd14cb6be54ee64e0e44161aa219
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2022-07-04 09:59:34 +0400

    .

--- pages/components/AboutMe.jsx
+++ pages/components/AboutMe.jsx
@@ -5,7 +5,10 @@ export default function AboutMe() {
         <h2>about:me</h2>
         <div>
             <p>Wrote my first lines of code in a cold 2007 autumn evening on a Windows 3.11 terminal. I’ve then been working in and out the IT since 2013.</p>
-            <p>I like to come up with creative solutions for my clients’ needs in many fields: despite my focus being on web development, my experiences range vastly from iOS development to system administration, from UX design to video/audio content production. Currently, I am tutoring and teaching for the Full Stack Course at <a href="https://strive.school" style={{ color: '#00ff84' }}>Epicode Global</a>, in which we introduce the MERN Stack to our students.</p>
+            <p>I like to come up with creative solutions for my clients’ needs in many fields: despite my focus being on web development, 
+                my experiences range vastly from iOS development to system administration, from UX design to video/audio content production. 
+                Between 2020 and 2022 I have tutored and taught in the Full Stack Course at <a href="https://strive.school">Epicode Global</a>, in which we introduced the MERN Stack to our students.</p>
+            <p>Currently, I am hired by <a href="https://reasonlabs.com">ReasonLabs</a> in the on-site Frontend team in Dubai, UAE.</p>
             <p>I have worked for people and companies in New York and Florida as well as worldwide: let’s chat together and I’ll tell you how I can help.</p>
         </div>
     </div>
