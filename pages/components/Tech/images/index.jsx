e86c083579868d40afe24e3c50783f6f69530784
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2023-06-07 19:13:38 +0400

    No more `<img>` tag

---
+++ pages/components/Tech/images/index.jsx
@@ -0,0 +1,29 @@
+import React from "./react.png";
+import Bootstrap from "./bootstrap.png";
+import SCSS from "./scss.png";
+import Redux from "./redux.png";
+import Recoil from "./recoil.png";
+import Angular from "./angular.png";
+import Typescript from "./typescript.png";
+import NodeJS from "./Node JS.png";
+import MongoDB from "./Mongo DB.png";
+import PostgreSQL from "./postgresql.png";
+import Solidity from "./solidity.png";
+import Hardhat from "./hardhat.png";
+
+const icons = {
+    React,
+    Bootstrap,
+    SCSS,
+    Redux,
+    Recoil,
+    Angular,
+    Typescript,
+    "Node JS": NodeJS,
+    "Mongo DB": MongoDB,
+    PostgreSQL,
+    Solidity,
+    Hardhat
+}
+
+export default icons
