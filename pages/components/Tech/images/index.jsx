cd618239251e33f93b2ed538a07dd3f6005ace59
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2023-06-07 19:15:23 +0400

    Updated imports

--- pages/components/Tech/images/index.jsx
+++ pages/components/Tech/images/index.jsx
@@ -1,15 +1,15 @@
-import React from "./react.png";
-import Bootstrap from "./bootstrap.png";
-import SCSS from "./scss.png";
-import Redux from "./redux.png";
-import Recoil from "./recoil.png";
-import Angular from "./angular.png";
-import Typescript from "./typescript.png";
+import React from "./React.png";
+import Bootstrap from "./Bootstrap.png";
+import SCSS from "./SCSS.png";
+import Redux from "./Redux.png";
+import Recoil from "./Recoil.png";
+import Angular from "./Angular.png";
+import Typescript from "./Typescript.png";
 import NodeJS from "./Node JS.png";
 import MongoDB from "./Mongo DB.png";
-import PostgreSQL from "./postgresql.png";
-import Solidity from "./solidity.png";
-import Hardhat from "./hardhat.png";
+import PostgreSQL from "./PostgreSQL.png";
+import Solidity from "./Solidity.png";
+import Hardhat from "./Hardhat.png";
 
 const icons = {
     React,
