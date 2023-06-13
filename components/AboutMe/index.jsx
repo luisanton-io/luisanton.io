8b8f42484e02af4a9d4fda29233e556409afa27a
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2023-06-13 17:42:50 +0400

    Update about:me

--- components/AboutMe/index.jsx
+++ components/AboutMe/index.jsx
@@ -10,19 +10,28 @@ export default function AboutMe() {
     return <div className={styles.aboutMe}>
         <h2>about:me</h2>
         <motion.div {...animationProps}>
-            <p>Wrote my first lines of code in a cold 2007 autumn evening on a Windows 3.11 terminal. I have then started working in the IT industry in 2013.</p>
             <p>
-                I like to come up with creative solutions for my clients’ needs in many fields: although my primary focus is web development,
-                my experiences range widely from iOS development to system administration, from UX design to video/audio content production.
+                Wrote my first lines of code in a cold 2007 autumn evening on a Windows 3.11 terminal.
+                I have then started working in the IT industry in 2013.
             </p>
             <p>
-                Recently, I have been immersing myself in Solidity and exploring the exciting possibilities of Web3 technologies.
+                Although my primary focus is web development, I have been recently immersing myself in
+                Solidity and exploring the exciting possibilities of Web3 technologies.
             </p>
-            <p>Regarding my most latest experiences, up until 2022 I have tutored and taught in the Full Stack Course at <a href="https://strive.school" target="_blank" rel="noreferrer">Epicode Global</a>, during which we introduced students to the MERN Stack.
+            <p>
+                Regarding my latest experiences, up until 2022 I have tutored and taught in the
+                Full Stack Course at <a href="https://strive.school" target="_blank" rel="noreferrer">Epicode Global</a>,
+                during which we introduced students to the MERN Stack.
             </p>
-            <p>Between 2022 and 2023, I was hired by <a href="https://reasonlabs.com" target="_blank" rel="noreferrer">ReasonLabs</a> as a member of the on-site Frontend team in Dubai, UAE.
+            <p>
+                Between 2022 and 2023, I was hired by <a href="https://reasonlabs.com" target="_blank" rel="noreferrer">ReasonLabs</a> as
+                a member of the on-site Frontend team in Dubai, where I have been working using
+                the latest available frontend technologies and maintaining their Electron client for <a href="https://reasonlabs.com/platform/products/endpoint-protection">RAV Endpoint Protection</a>.
             </p>
-            <p>I have worked for individuals and organizations in New York and Florida as well as worldwide: let’s chat together and I’ll tell you how I can help.</p>
+            <p>
+                I have worked for individuals and organizations in New York and Florida as well
+                as worldwide: let’s chat together and I’ll tell you how I can help.
+            </p>
         </motion.div>
     </div>
 }
