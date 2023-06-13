5f9b87f6db31049ff2d67db3d67e26dcf1c77f13
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2023-06-13 17:43:02 +0400

    Update hero

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react"
-import styles from "./styles.module.scss"
 import Image from "next/image"
+import { useEffect, useState } from "react"
 import me from "./me.png"
+import styles from "./styles.module.scss"
 
 export default function Hero() {
     const [tint, setTint] = useState(0)
@@ -12,7 +12,7 @@ export default function Hero() {
         }, 150)
     }, [])
 
-    return <div className={styles.hero} >
+    return <div className={styles.hero}>
         <div className="scale-in-center">
             <div className={styles.imgWrapper}>
                 <div style={{ '--tint': tint }}>
@@ -20,9 +20,26 @@ export default function Hero() {
                 </div>
             </div>
         </div>
-        <div className="slide-in-blurred-bottom">
-            <h1> Hello. < br /> My name is Luis Antonio.</h1 >
-            <h4>Full Stack Developer</h4>
+        <div className={styles.headerText}>
+            <div className="slide-in-blurred-bottom">
+                <h1> Hello. <br /> My name is Luis Antonio.</h1 >
+                <h4>Full Stack Developer</h4>
+            </div>
+            <div className={styles.linksWrapper}>
+                <a style={{ animationDelay: '1s' }} target="_blank" href="https://linkedin.com/in/luis-antonio-canettoli">
+                    <Image alt="Linked In" src="/linkedin.png" width={84} height={21} />
+                    <span>LinkedIn</span>
+                </a>
+                <a style={{ animationDelay: '1.25s' }} target="_blank" href="https://github.com/luisanton-io">
+                    <Image alt="github" src="/github.svg" width={20} height={20} />
+                    <span>Github</span>
+                    <Image alt="github" src="/github.png" width={75} height={31} />
+                </a>
+                <a style={{ animationDelay: '1.5s' }} target="_blank" href="https://stackoverflow.com/users/11783958/luis-antonio-canettoli-ordo%c3%b1ez">
+                    <Image alt="Stack Overflow" src="/stack-overflow.svg" width={155} height={30} style={{ marginBottom: '0.4em' }} />
+                    <span>Stack Overflow</span>
+                </a>
+            </div>
         </div>
-    </div >
+    </div>
 }
