69a6c562076cd4e17e1e80639c506b315451b294
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2026-01-17 14:50:12 +0400

    add cv redirect

--- next.config.js
+++ next.config.js
@@ -4,5 +4,15 @@ module.exports = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/cv',
+        destination: 'https://drive.google.com/file/d/1d4-2LEnHhnjLB6ox6btH9njXeiYQmD8H/view',
+        permanent: false,
+      },
+    ]
+  },
+
 }
